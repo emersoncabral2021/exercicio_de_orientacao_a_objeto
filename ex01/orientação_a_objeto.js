58db3a139e122498daaf954da534d394aecd8704
emerson da conceição cabral <emersoncabral202@gmail.com>
2021-07-08 22:22:35 -0300

    ajuste

--- ex01/orientação_a_objeto.js
+++ ex01/orientação_a_objeto.js
@@ -1,9 +1,16 @@
 var ingrendiente = [
     'mel','água','sal','mostarda'
 ]
-ingrendiente.sort()
+
+function ordenado(){
+    ingrendiente.sort(function(a,b){
+    return a.localeCompare(b)
+})
 
 ingrendiente.forEach(function(el){
     console.log(el.charAt(0).toUpperCase() + el.substr(1))
 
 })
+
+}
+ordenado()
